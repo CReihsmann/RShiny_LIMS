8934083608ea0b559a4e784e76f128b7c121664a
Conrad <isletcore@administorsimac.dhcp.mc.vanderbilt.edu>
2022-12-07 15:23:20 -0600

    plot experimenting

--- notebooks/20221207-plot_testing.R
+++ notebooks/20221207-plot_testing.R
@@ -4,7 +4,11 @@ library(plotly)
 
 lims_data <- read_csv('../data/20221207-fixed_cols.csv')
 
-lims_data <- lims_data %>% 
+graph_cols <- c('DATE_PANCREAS_ISLET_RECEIVED','ISLETS_ISOLATION_CENTER','CI_DONOR_RACE',
+                'CI_DONOR_DISEASE','CI_DONOR_GENDER','AGE_IN_YEARS', 'CI_DONOR_BMI_MANUAL')
+
+graph_data <- lims_data %>% 
+  select(contains(graph_cols)) %>% 
   mutate(AGE_GROUPS = if_else((AGE_IN_YEARS > 0&AGE_IN_YEARS <= 10), '1-10',
                               if_else((AGE_IN_YEARS > 10&AGE_IN_YEARS <= 20), '11-20',
                                       if_else((AGE_IN_YEARS > 20&AGE_IN_YEARS <= 30), '21-30',
@@ -14,12 +18,59 @@ lims_data <- lims_data %>%
                                                                       if_else((AGE_IN_YEARS > 60&AGE_IN_YEARS <= 70), '61-70',
                                                                               if_else((AGE_IN_YEARS > 70&AGE_IN_YEARS <= 80), '71-80',
                                                                                       if_else((AGE_IN_YEARS > 80&AGE_IN_YEARS <= 90), '81-90',
-                                                                                              if_else((AGE_IN_YEARS > 90&AGE_IN_YEARS <= 100), '91-100', 'NA')))))))))))
-
-lims_data %>% 
-  ggplot(aes(x = fct_rev(fct_infreq(ISLETS_ISOLATION_CENTER, ordered = F)), fill = CI_DONOR_RACE)) +
+                                                                                              if_else((AGE_IN_YEARS > 90&AGE_IN_YEARS <= 100), '91-100', 'NA'))))))))))) %>% 
+  mutate(YEAR = as.character(year(DATE_PANCREAS_ISLET_RECEIVED)))
+#-----Isolation Center bar
+graph_data %>% 
+  ggplot(aes(x = fct_infreq(ISLETS_ISOLATION_CENTER, ordered = F), fill = CI_DONOR_RACE)) +
   geom_bar()+
-  coord_flip()
+  coord_flip() +
+  labs(x = "Isolation Center")+
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 20)) +
+  scale_fill_discrete(labels = function(x) str_wrap(x, width = 20)) +
+  theme(legend.spacing.y = unit(0.15, 'cm')) +
+  guides(fill = guide_legend(byrow = T))
+
+#-----Disease State bar
+lims_data %>% 
+  ggplot(aes(x = fct_infreq(CI_DONOR_DISEASE), fill = CI_DONOR_GENDER)) +
+  geom_bar()+
+  coord_flip() +
+  labs(x = "Isolation Center") +
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 20)) +
+  scale_fill_discrete(labels = function(x) str_wrap(x, width = 20)) +
+  theme(legend.spacing.y = unit(0.15, 'cm')) +
+  guides(fill = guide_legend(byrow = T))
+
+#-----Age Groups
+lims_data %>% 
+  ggplot(aes(x = fct_infreq(AGE_GROUPS, ordered = F), fill = ISLETS_ISOLATION_CENTER)) +
+  geom_bar()+
+  coord_flip() +
+  labs(x = "Age Groups")+
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 20)) +
+  scale_fill_discrete(labels = function(x) str_wrap(x, width = 20)) +
+  theme(legend.spacing.y = unit(0.15, 'cm')) +
+  guides(fill = guide_legend(byrow = T))
+
+#-----Years
+ggplotly(graph_data %>% 
+  ggplot(aes(x = YEAR, fill = ISLETS_ISOLATION_CENTER)) +
+  geom_bar()+
+  coord_flip() +
+  labs(x = "Year")+
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 20)) +
+  scale_fill_discrete(labels = function(x) str_wrap(x, width = 20)))
+
+
+test <- lims_data %>% 
+  select()
+  group_by(YEAR) %>% 
+  summarise()
+  
+  
+  plot_ly(x = ~fct_infreq(ISLETS_ISOLATION_CENTER), y = ~ISLETS_ISOLATION_CENTER, type = 'bar', color = ~CI_DONOR_RACE) %>% 
+  layout(barmode = 'stack')
 
 lims_data %>% 
   ggplot(aes(x = AGE_IN_YEARS)) + 
